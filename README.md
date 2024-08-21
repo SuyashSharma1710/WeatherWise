7ed7cb4a3a6661de4ba7f742136555b472b62445
SuyashSharma1710 <SuyashSharma1710@users.noreply.github.com>
2024-08-21 18:26:25 +0000

    deploy: 6d694cf4b786a261e45621180bf08abeb70a83fa

---
+++ README.md
@@ -0,0 +1,35 @@
+# WeatherWise
+
+**WeatherWise** is a sleek and modern weather application that provides users with real-time weather updates and forecasts. It presents key weather details like temperature, wind, UV index, and a 10-day forecast in an elegant user interface. Designed with a responsive layout, WeatherWise ensures a seamless experience across different screen sizes and devices.
+
+## Features
+
+- **Real-time Weather Updates**: Get current temperature, weather conditions, and forecasts.
+- **Hourly Forecast**: View hourly weather data including temperature and conditions.
+- **10-day Forecast**: Detailed day-by-day weather forecasts.
+- **Wind & UV Index**: Displays current wind speed and UV index with graphical representations.
+- **Responsive Design**: Adapts beautifully to different devices, ensuring a great experience on mobile and desktop.
+
+## Technologies Used
+
+- **HTML5**: Structuring the web content.
+- **CSS3**: Styling the layout and appearance, with responsive designs for mobile devices.
+- **JavaScript**: Enabling dynamic content and responsive behavior.
+- **Media Queries**: Ensuring the layout adjusts based on screen sizes.
+- **Weather API**: Fetching real-time weather data from an external source.
+
+## Getting Started
+
+### Prerequisites
+
+To run WeatherWise on your local machine, you’ll need:
+
+- A modern web browser (Google Chrome, Mozilla Firefox, etc.)
+- Internet connection for fetching live weather data.
+
+### Installing and Running
+
+1. **Clone the repository**:
+
+   ```bash
+   git clone https://github.com/SuyashSharma1710/WeatherWise.git
---
+++ script.js
@@ -0,0 +1,200 @@
+  
+  const apiKey = "ea92aa70665b927dd92729d7b6fe8fd6";
+
+  document.getElementById("city").addEventListener("keydown", function (event) {
+    if (event.key === "Enter") {
+      const city = document.getElementById("city").value;
+
+      // Use fetch to get latitude and longitude for the city
+      fetch(
+        `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`
+      )
+        .then((response) => response.json())
+        .then((data) => {
+          const lat = data.coord.lat;
+          const lon = data.coord.lon;
+
+          // Use lat and lon to get weather data
+          fetch(
+            `https://api.openweathermap.org/data/3.0/onecall?lat=${lat}&lon=${lon}&appid=${apiKey}&units=metric`
+          )
+            .then((response) => response.json())
+            .then((weatherData) => {
+              citynamechanger();
+              console.log(weatherData); // Handle and display the weather data
+              displayWeather(weatherData);
+            });
+        })
+        .catch((error) => console.error("Error fetching weather data:", error));
+    }
+  });
+
+  function citynamechanger() {
+    const cityname = document.getElementById("city").value;
+    document.getElementById("cityname").innerHTML = `<span>${cityname}</span>`;
+  }
+
+  function displayWeather(data) {
+    if (data.current.temp) {
+      document.getElementById("temperature").innerHTML = `${Number(
+        data.current.temp.toFixed(1)
+      )}<span>°</span>`;
+    }
+    if (data.current.temp) {
+      document.getElementById("temperature").innerHTML = `${Number(
+        data.current.temp.toFixed(1)
+      )}<span>°</span>`;
+    }
+
+    if (
+      data.current.weather &&
+      data.current.weather[0] &&
+      data.current.weather[0].main
+    ) {
+      document.getElementById(
+        "weather"
+      ).innerHTML = `${data.current.weather[0].main}`;
+    }
+
+    if (data.lat) {
+      document.getElementById("lat").innerHTML = `lat: ${data.lat}`;
+    }
+
+    if (data.lon) {
+      document.getElementById("lon").innerHTML = `lon: ${data.lon},`;
+    }
+
+    if (data.current.wind_speed) {
+      document.getElementById("windd").innerHTML = `${Number(
+        data.current.wind_speed.toFixed(1)
+      )}`;
+    }
+
+    if (data.current.wind_gust) {
+      document.getElementById("gustd").innerHTML = `${Number(
+        data.current.wind_gust.toFixed(1)
+      )}`;
+    }
+
+    if (data.current.visibility) {
+      document.getElementById("visibility").innerHTML = `${visibility(
+        data.current.visibility
+      )}km`;
+    }
+
+    if (data.current.feels_like) {
+      document.getElementById("feelslike").innerHTML = `${Number(
+        data.current.feels_like.toFixed(1)
+      )}°`;
+    }
+
+    if (data.current.humidity) {
+      document.getElementById("humidity").innerHTML = `${Number(
+        data.current.humidity.toFixed(1)
+      )}%`;
+    }
+
+    if (data.current.pressure) {
+      document.getElementById(
+        "pressure"
+      ).innerHTML = `${data.current.pressure}<span style="font-size: 10px;">Hg</span>`;
+    }
+
+    if (data.current.uvi) {
+      document.getElementById("uvnum").innerHTML = `${data.current.uvi}`;
+      document.getElementById("uvtype").innerHTML = `${uvi(data)}`;
+    }
+
+    hourlydata(data);
+    dailydata(data);
+  }
+
+  function visibility(data) {
+    let vis = data;
+    vis = vis / 1000;
+    return Number(vis.toFixed(1));
+  }
+
+  function uvi(data) {
+    const uv = data.current.uvi;
+    if (uv >= 0 && uv <= 2) {
+      return "Low";
+    } else if (uv >= 3 && uv <= 5) {
+      return "Moderate";
+    } else if (uv >= 6 && uv <= 7) {
+      return "High";
+    } else if (uv >= 8 && uv <= 10) {
+      return "Very High";
+    } else if (uv >= 11) {
+      return "Extreme";
+    } else {
+      return "Invalid UVI";
+    }
+  }
+
+  function hourlydata(data) {
+    let hourlydt = "";
+    for (i = 0; i <= 20; i++) {
+      const timestamp = data.hourly[i].dt;
+      const temp = data.hourly[i].temp;
+      const date = new Date(timestamp * 1000);
+
+      const hours = date.getUTCHours().toString().padStart(2, "0"); // Get hours and pad with leading zero if needed
+      const minutes = date.getUTCMinutes().toString().padStart(2, "0"); // Get minutes and pad with leading zero if needed
+
+      const time = `${hours}:${minutes}`;
+      hourlydt += ` <div id="hdata">
+                <p>${time}</p>
+                <p>${Number(temp.toFixed(1))}°</p>
+              </div>`;
+    }
+
+    document.getElementById("hourlydatablk").innerHTML = hourlydt;
+  }
+  function dailydata(data) {
+    let dailydt = "";
+    for (i = 0; i <= 7; i++) {
+      const timestamp = data.daily[i].dt;
+      const temp = data.daily[i].temp.day;
+      const date = new Date(timestamp * 1000);
+
+      const hours = date.getUTCHours().toString().padStart(2, "0"); // Get hours and pad with leading zero if needed
+      const minutes = date.getUTCMinutes().toString().padStart(2, "0"); // Get minutes and pad with leading zero if needed
+      const day = date.getUTCDate().toString().padStart(2, "0"); // Get day and pad with leading zero
+      const month = (date.getUTCMonth() + 1).toString().padStart(2, "0"); // Get month and pad with leading zero
+
+      const formattedDate = `${day}/${month}`;
+      const time = `${hours}:${minutes}`;
+
+      dailydt += `  <div id="ddata">
+                <p>${formattedDate}</p>
+                <p>${time}</p>
+                <p>${Number(temp.toFixed(1))}°</p>
+              </div>`;
+    }
+
+    document.getElementById("dailydatablk").innerHTML = dailydt;
+  }
+
+  const hourlyDataBlk = document.getElementById("hourlydatablk");
+  hourlyDataBlk.addEventListener("wheel", (event) => {
+    event.preventDefault();
+    hourlyDataBlk.scrollLeft += event.deltaY * 2;
+  });
+
+  window.addEventListener('resize', function() {
+    var stylesheet = document.getElementById("mainStylesheet");
+    if (window.innerWidth <= 920) {
+      stylesheet.disabled = true;
+    } else {
+      stylesheet.disabled = false;
+    }
+  });
+
+  // Initial check on page load
+  window.onload = function() {
+    var stylesheet = document.getElementById("mainStylesheet");
+    if (window.innerWidth <= 920) {
+      stylesheet.disabled = true;
+    }
+  };
